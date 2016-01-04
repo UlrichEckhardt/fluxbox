0c7aef9c87446ea77dbf7938316707dc0b360df6
Ulrich Eckhardt <ulrich.eckhardt@base-42.de>
2016-01-04 10:50:06 +0100

    reactivate parsertest.cc

--- src/tests/parsertest.cc
+++ src/tests/parsertest.cc
@@ -4,31 +4,28 @@
 #include "../FbMenuParser.hh"
 
 #include <iostream>
+#include <stdlib.h>
 
 using namespace std;
-
+using namespace FbTk;
 
 int main(int argc, char **argv) {
     if (argc < 2) {
         cerr<<"you must supply an argument!"<<endl;
-        exit(0);
+        return EXIT_FAILURE;
     }
     cerr<<"Loading: "<<argv[1]<<endl;
-    Parser *p = new FbMenuParser(argv[1]);
-    if (!p->isLoaded()) {
+    FbMenuParser p(argv[1]);
+    if (!p.isLoaded()) {
         cerr<<"Couldn't load file: "<<argv[1]<<endl;
-        delete p;
-        exit(0);
+        return EXIT_FAILURE;
     }
 
     Parser::Item item, item2, item3;
-    int args = 0;
-    int last_line_num = 0;
     std::string last_line;
     std::string type, name, argument;
-    while (!p->eof()) {
-        (*p)>>item>>item2>>item3;
-        cerr<<item.second<<","<<item2.second<<", "<<item3.second<<endl;
+    while (!p.eof()) {
+        p>>item>>item2>>item3;
+        cout<<item.second<<","<<item2.second<<", "<<item3.second<<endl;
     }
-    delete p;
 }
